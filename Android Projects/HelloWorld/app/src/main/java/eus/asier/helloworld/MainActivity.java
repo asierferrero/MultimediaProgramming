2d97f70bbf474096d1bd7e348177d9647906e94b
asierferrero <ferrero.asier@uni.eus>
2023-09-13 21:33:37 +0200

    13/09/2023

--- Android Projects/HelloWorld/app/src/main/java/eus/asier/helloworld/MainActivity.java
+++ Android Projects/HelloWorld/app/src/main/java/eus/asier/helloworld/MainActivity.java
@@ -3,12 +3,30 @@ package eus.asier.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int counter;
+    Button incrementButton;
+    TextView counterTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //This call sets the layout to activity_main.xml
+
+        incrementButton = findViewById(R.id.increaseCounterBtn);
+        counterTextView = findViewById(R.id.couterClicksText);
+
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // increase the counter
+                // set the counter into the TextView
+                counter++;
+                counterTextView.setText("You have clicked " + counter + " times");
+            }
+        });
     }
 }
